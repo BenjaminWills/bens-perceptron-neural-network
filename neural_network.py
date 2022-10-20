3309cda48c4cc33d40c87f67ce9cc48d383b7e1c
BenjaminWills <benjaminwills047@gmail.com>
2022-10-20 21:59:48 +0100

    Added output calculator

--- neural_network.py
+++ neural_network.py
@@ -1,19 +1,18 @@
 from Mathematics_fundamentals.functions.functions import Functions
 from Mathematics_fundamentals.linear_algebra.linear_algebra import (Matrix,
                                                                     Vector)
-
+import numpy as np
 
 class Layer:
-    def __init__(self,nodes_in:int,nodes_out:int,weights_in:Matrix,biases:Vector,input:Vector) -> None:
+    def __init__(self,nodes_in:int,nodes_out:int,weights_in:Matrix,biases:Vector) -> None:
         self.nodes_in = nodes_in
         self.nodes_out = nodes_out
         self.weights = weights_in
         self.biases = biases
-        self.input = input
         self.output_weights = self.get_outputs()
 
-    def get_outputs(self):
-        weight_vector = self.weights * self.input + self.biases
+    def get_outputs(self,input:Vector) -> Vector:
+        weight_vector = self.weights * input + self.biases
         sigmoid_vector = Vector(
             [Functions.sigmoid(component) for component in Vector.unpack_vector(weight_vector)]
         )
@@ -23,8 +22,14 @@ class Neural_Network:
     def __init__(self,layers:Vector) -> None:
         self.layers = layers
 
-    def create_network(self):
-        pass
+    def calculate_output(self,input:Vector) -> Vector:
+        for layer in self.layers:
+            input = layer.get_outputs(input)
+        return input
+
+    def classify_output(self,input:Vector) -> float:
+        outputs = self.calculate_output(input)
+        return np.argmax(Vector.unpack_vector(outputs))
 
 
 if __name__ == "__main__":
