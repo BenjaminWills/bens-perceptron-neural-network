195e633d46fd10bb60ba1fdf3214e4d1c444d723
BenjaminWills <benjaminwills047@gmail.com>
2022-10-20 22:51:20 +0100

    Randomised wights + biases

--- neural_network.py
+++ neural_network.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import numpy as np
 
 from Mathematics_fundamentals.functions.functions import Functions
@@ -6,49 +8,51 @@ from Mathematics_fundamentals.linear_algebra.linear_algebra import (Matrix,
 
 
 class Layer:
-    def __init__(self,nodes_in:int,nodes_out:int,weights_in:Matrix,biases:Vector) -> None:
+    def __init__(self,nodes_in:int,nodes_out:int,inputs:Vector) -> None:
         self.nodes_in = nodes_in
         self.nodes_out = nodes_out
-        self.weights = weights_in
-        self.biases = biases
+        self.inputs = inputs
+        self.weights = self.get_random_weights()
+        self.biases = self.get_random_biases()
         self.output_weights = self.get_outputs()
 
-    def get_outputs(self,input:Vector) -> Vector:
-        weight_vector = self.weights * input + self.biases
+    def get_random_weights(self):
+        random_matrix = [[np.random.uniform(-10,10) for i in range(self.nodes_in)] for j in range(self.nodes_out)]
+        return Matrix(*random_matrix)
+    def get_random_biases(self):
+        random_list = [np.random.uniform(-10,10) for i in range(self.nodes_in)]
+        return Vector(*random_list)
+
+    def get_outputs(self) -> Vector:
+        weight_vector = self.weights * self.inputs + self.biases
         sigmoid_vector = Vector(
             [Functions.sigmoid(component) for component in Vector.unpack_vector(weight_vector)]
         )
         return sigmoid_vector
 
 class Neural_Network:
-    def __init__(self,layers:Vector) -> None:
-        self.layers = layers
+    def __init__(self,inputs:Vector,*layer_sizes:List[int]) -> None:
+        self.layer_sizes = layer_sizes
+        self.inputs = inputs
+        self.layers = self.create_network()
 
-    def calculate_output(self,input:Vector) -> Vector:
-        for layer in self.layers:
-            input = layer.get_outputs(input)
-        return input
+    def create_network(self):
+        layers = []
+        inputs = self.inputs
+        for i in range(len(self.layer_sizes)):
+            new_layer = Layer(
+                        self.layer_sizes[i],
+                        self.layer_sizes[i+1],
+                        inputs,
+                        )
+            inputs = new_layer.get_outputs()
+            layers.append(new_layer)
 
-    def classify_output(self,input:Vector) -> float:
-        outputs = self.calculate_output(input)
+    def classify_output(self) -> float:
+        outputs = self.layers[self.layer_sizes[-1]].get_outputs()
         return np.argmax(Vector.unpack_vector(outputs))
 
 
 if __name__ == "__main__":
-    layer = Layer(
-        2,
-        2,
-        Matrix(
-            [0.5,0.5],
-            [0.5,0.5]
-            ),
-        Vector(
-            1,
-            1
-        ),
-        Vector(
-            1,
-            1
-        )
-        )
-    layer.weights.show_matrix()
+    network = Neural_Network(Vector(1,1),2,3,2)
+    
