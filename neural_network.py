bfb65985e7de53154926319afbbf86687aeb9873
BenjaminWills <benjaminwills047@gmail.com>
2022-10-20 21:30:05 +0100

    Added initial skeleton code

--- neural_network.py
+++ neural_network.py
@@ -1,3 +1,14 @@
+from Mathematics_fundamentals.linear_algebra.linear_algebra import (Matrix,
+                                                                    Vector)
+
+
+class Layer:
+    def __init__(self,nodes_in:int,nodes_out:int,weights:Matrix,biases:Vector) -> None:
+        self.nodes_in = nodes_in
+        self.nodes_out = nodes_out
+        self.weights = weights
+        self.biases = biases
+
 class Neural_Network:
-    def __init__(self,):
-        pass
+    def __init__(self,layers:Vector) -> None:
+        self.layers = layers
